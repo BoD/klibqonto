ead2df1c202f8d7ffc846ba150f36b5e82f25180
BoD <BoD@JRAF.org>
2020-12-12 19:02:01 +0100

    Migrate to gradle kts

---
+++ build.gradle.kts
@@ -0,0 +1,66 @@
+import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
+
+plugins {
+    id("com.github.ben-manes.versions") version Versions.BEN_MANES_VERSIONS_PLUGIN
+}
+
+buildscript {
+    repositories {
+        google()
+        jcenter()
+    }
+
+    dependencies {
+        classpath("com.android.tools.build", "gradle", Versions.ANDROID_GRADLE_PLUGIN)
+        classpath(kotlin("gradle-plugin", Versions.KOTLIN))
+        classpath("com.github.ben-manes", "gradle-versions-plugin", Versions.BEN_MANES_VERSIONS_PLUGIN)
+        classpath("org.jetbrains.dokka", "dokka-gradle-plugin", Versions.DOKKA_PLUGIN)
+    }
+}
+
+allprojects {
+    repositories {
+        mavenLocal()
+        google()
+        jcenter()
+    }
+
+    group = "org.jraf"
+    version = "2.1.1"
+
+    // Show a report in the log when running tests
+    tasks.withType<Test> {
+        testLogging {
+            events("passed", "skipped", "failed", "standardOut", "standardError")
+        }
+    }
+}
+
+tasks {
+    register<Delete>("clean") {
+        delete(rootProject.buildDir)
+    }
+
+    wrapper {
+        distributionType = Wrapper.DistributionType.ALL
+        gradleVersion = Versions.GRADLE
+    }
+
+    // Configuration for gradle-versions-plugin
+    // Run `./gradlew dependencyUpdates` to see latest versions of dependencies
+    withType<DependencyUpdatesTask> {
+        resolutionStrategy {
+            componentSelection {
+                all {
+                    if (
+                        setOf("alpha", /*"beta", "rc",*/ "preview", "eap", "m1").any {
+                            candidate.version.contains(it, true)
+                        }
+                    ) {
+                        reject("Non stable")
+                    }
+                }
+            }
+        }
+    }
+}
